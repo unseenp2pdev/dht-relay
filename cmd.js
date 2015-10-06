c5bef41c6c1c93a19203ce785a105a82b25e30b4
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-10-06 13:20:16 +0100

    fix typo

--- cmd.js
+++ cmd.js
@@ -13,8 +13,8 @@ var argv = require('minimist')(process.argv.slice(2), {
   ]
 })
 
-var createRelay = argv.dht ? require('./dht-relay') : require('./proxy').createServer
+var createRelay = argv.dht ? require('./dht-relay') : require('./relay').createServer
 var port = Number(argv.port) || 25778
-var udpProxy = proxy.createServer(port)
+var udpProxy = createRelay(port)
 
 console.log('Running on port', port)
